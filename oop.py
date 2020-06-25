fab3f4dfba4da5746d68c221cfa8ab0a36c3c50d
alizada121 <58678445+alizada121@users.noreply.github.com>
2020-06-25 15:29:06 +0300

    oop.py

---
+++ oop.py
@@ -0,0 +1,62 @@
+class Employee:
+
+    num_of_emps = 0
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+    #private instance
+    def __init__(self, name, last):
+        self.__name=name  # private attribute 
+        self.__last=last # private attribute
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return 
+        
+     @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
+
+
+emp_1 = Employee('Alizada', 'Leman', 50000)
+emp_2 = Employee('Babazade', 'Sabina', 60000)
+
+Employee.set_raise_amt(1.05)
+
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)
+
+emp_str_1 = 'Alizada-Leman-70000'
+emp_str_2 = 'Babazade-Nezrin-30000'
+emp_str_3 = 'Sabina-Abraham-90000'
